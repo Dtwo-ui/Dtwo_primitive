6a8a35a7bce334be315363401811495fe464fc3f
dott <hyeongman24@gmail.com>
2023-11-06 16:47:22 +0900

    feat: primitive 컴포넌트 타입 설계 및 구현

---
+++ packages/primitives/primitives/primitives.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const nodes = [
+  'a',
+  'button',
+  'div',
+  'form',
+  'h1',
+  'h2',
+  'h3',
+  'img',
+  'input',
+  'label',
+  'li',
+  'nav',
+  'ol',
+  'p',
+  'span',
+  'svg',
+  'ul',
+] as const;
+
+type PrimitivesType = {
+  [T in (typeof nodes)[number]]: React.ForwardRefExoticComponent<
+    React.PropsWithoutRef<React.ComponentProps<T>>
+  >;
+};
+
+export const Primitive = nodes.reduce((components, htmlElement) => {
+  const Node = React.forwardRef(
+    (props: React.PropsWithoutRef<React.ComponentProps<typeof htmlElement>>, ref: any) => {
+      const Component: any = htmlElement;
+
+      return (
+        <Component {...props} ref={ref}>
+          {props.children}
+        </Component>
+      );
+    }
+  );
+
+  return { ...components, [htmlElement]: Node };
+}, {} as PrimitivesType);
